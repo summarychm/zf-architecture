c5267baa5cbe2e4bdcdefb207318d9ad64e6fd83
max.liu <summarychm@hotmail.com>
2019-09-28 13:28:45 +0800

    支持effects副作用action

--- 19.dva-source/src/dva/index.js
+++ 19.dva-source/src/dva/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import {createStore, combineReducers} from "redux";
+import {createStore, combineReducers, applyMiddleware} from "redux";
 import {connect, Provider} from 'react-redux';
+import createSagaMiddleware from "redux-saga";
+import * as sagaEffects from "redux-saga/effects";
 export {connect};
+
 
 export default function () {
   const app = {
@@ -34,17 +37,37 @@ export default function () {
         return state;
       }
     });
+
+    function* rootSaga() {
+      let {takeEvery} = sagaEffects;
+      // 批量注册model实例中的effects
+      for (const model of app._models) {
+        for (const key in model.effects) {
+          let name = model.namespace + "/" + key;
+          // 使用takeEvery监听当前namespace下的effects
+          yield takeEvery(name, function* (action) {
+            yield model.effects[key](action, sagaEffects);
+          });
+        }
+      }
+    }
+
     let rootReducer = combineReducers(reducers);// 合并rootReducer
-    let store = createStore(rootReducer);
+    let sagaMiddleware = createSagaMiddleware();// 创建saga中间件
+    let store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
+    sagaMiddleware.run(rootSaga);// 开启saga中间件监听
+    
+
     let App = app._router; // 重命名router信息
     ReactDOM.render(
-    <Provider store={store}>
-      <App />
-    </Provider>, document.querySelector(selector));
+      <Provider store={store}>
+        <App />
+      </Provider>, document.querySelector(selector));
   }
   return app;
 }
-function prefixNamespace(reducers, namespace, NAMESPACE_SEPARATOR="/") {
+// TODO 可抽取为utils
+function prefixNamespace(reducers, namespace, NAMESPACE_SEPARATOR = "/") {
   // 专门处理数组类型的reducer
   let result = Object.keys(reducers).reduce((memo, key) => {
     const newKey = `${namespace}${NAMESPACE_SEPARATOR}${key}`;
--- 19.dva-source/src/index.js
+++ 19.dva-source/src/index.js
@@ -1,19 +1,33 @@
 import React from 'react';
-import dva,{connect} from './dva';
+import dva, {connect} from './dva';
 
-let app=new dva();
+// TODO 可抽取为utils
+function delay(ms) {
+  return new Promise((resolve, reject) => {
+    setTimeout(resolve, ms);
+  })
+}
+
+let app = new dva();
 app.model({
-  namespace:"counter",
-  state:{number:0},
-  reducers:{
-    add(state=0,{payload=1}){
-      return {number:state.number+payload}
+  namespace: "counter",
+  state: {number: 0},
+  reducers: {
+    add(state = 0, {payload = 1}) {
+      return {number: state.number + payload}
+    }
+  },
+  effects: {
+    *asyncAdd({payload}, {call, put}) {
+      yield call(delay, 500);
+      yield put({type: "counter/add", payload});
     }
   }
 });
-const Counter=connect(state=>state.counter)(props=>(<>
+const Counter = connect(state => state.counter)(props => (<>
   <p>{props.number}</p>
-  <button onClick={()=>props.dispatch({type:"counter/add",payload:6})}>+</button>
+  <button onClick={() => props.dispatch({type: "counter/add", payload: 6})}>+</button>
+  <button onClick={() => props.dispatch({type: "counter/asyncAdd",payload:10})} >asyncAdd</button>
 </>));
-app.router(()=><Counter/>);
+app.router(() => <Counter />);
 app.start("#root");
